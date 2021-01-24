fc0324e173ddaed84f8c78f52dbbf202b1e665dd
yoooolim <agada937@naver.com>
2021-01-24 20:31:14 +0900

    commit

--- src/Input_and_Output/BOJ_10992.java
+++ src/Input_and_Output/BOJ_10992.java
@@ -10,6 +10,7 @@ public class BOJ_10992 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+
         int n = Integer.parseInt(br.readLine());
         for(int i=0;i<n-1;i++){
             for(int j=n-i-1;j>0;j--) bw.write(' ');
