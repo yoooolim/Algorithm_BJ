1e7ef8040b9f07e039eb15cee0d8ac5b741502be
yoooolim <agada937@naver.com>
2021-02-23 23:04:39 +0900

    11053_middle commit

---
+++ src/DP/BOJ_11053.java
@@ -0,0 +1,14 @@
+package DP;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_11053 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String input = br.readLine();
+        int [] seq = new int[n];
+        //seq=Integer.parseInt(input.split(" "));
+    }
+}
