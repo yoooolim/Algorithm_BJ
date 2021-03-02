adc726969e49393920472ad69619e0f19458a67b
yoooolim <agada937@naver.com>
2021-03-03 00:33:46 +0900

    11054_finish_commit

--- src/DP/BOJ_11054.java
+++ src/DP/BOJ_11054.java
@@ -11,7 +11,24 @@ public class BOJ_11054 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int [] input = new int[n];
         for(int i=0;i<n;i++) input[i]=Integer.parseInt(st.nextToken(" "));
+        int[] asc = new int[n];
+        int[] desc = new int[n];
         int[] dp = new int[n];
-        dp[0]=1;
+        asc[0]=1;
+        desc[n-1]=1;
+        int max = 0;
+        for(int i=1;i<n;i++){
+            asc[i]=1;
+            desc[n-i-1]=1;
+            for(int j=0;j<n;j++){
+                if(input[i]>input[j]&&asc[i]<=asc[j]) asc[i]=asc[j]+1;
+                if(input[n-i-1]>input[n-j-1]&&desc[n-i-1]<=desc[n-j-1]) desc[n-i-1]=desc[n-j-1]+1;
+            }
+        }
+        for(int i=0;i<n;i++){
+            dp[i]=asc[i]+desc[i]-1;
+            max=Math.max(dp[i],max);
+        }
+        System.out.println(max);
     }
 }
