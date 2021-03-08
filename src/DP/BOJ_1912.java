637add5d9da3721a480eb6f609d42c4882f10123
yoooolim <agada937@naver.com>
2021-03-08 23:12:53 +0900

    1912_middle_commit

--- src/DP/BOJ_1912.java
+++ src/DP/BOJ_1912.java
@@ -7,7 +7,7 @@ import java.util.StringTokenizer;
 /*
     dp[n+1][n+1] -> 메모리 초과
         ==> 슬라이딩 배열 dp[2][n+1] -> 바로 위의 값만 필요하기 때문
-    슬라이딩 배열 -> 시간 초과 어쩌라는거쥐 ㅠㅠㅠ
+    슬라이딩 배열 -> 시간 초과 어쩌라는거쥐 ㅠㅠㅠ -> 다른 방법을 찾으라는 거겠지
 */
 
 public class BOJ_1912 {
@@ -15,10 +15,25 @@ public class BOJ_1912 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int [] seq = new int[n+1];
+        int [] seq = new int[n];
+        for(int i=0;i<n;i++) seq[i]=Integer.parseInt(st.nextToken(" "));
+        int[][] dp = new int[2][n];
+        int max = 0;
+        for(int i=0;i<n;i++){
+            if(seq[i]<=0) continue;
+            for(int j=0;j<n;j++) {
+                if(j==0) dp[i%2][j]=seq[i];
+                else{
+                    dp[i % 2][j] = dp[i%2][j-1]+seq[j];
+                }
+                max = Math.max(dp[i%2][j],max);
+            }
+
+        }
+        System.out.print(max);
+        /*
         int [][] dp = new int[2][n+1];
         seq[0]=0;
-        for(int i=1;i<n+1;i++) seq[i]=Integer.parseInt(st.nextToken(" "));
         for(int i=0;i<n+1;i++) dp[0][i]=seq[i];
         int max = 0;
         for(int i=1;i<n+1;i++){
@@ -27,6 +42,6 @@ public class BOJ_1912 {
                 max=Math.max(max,dp[i%2][j]);
             }
         }
-        System.out.println(max);
+        System.out.println(max);*/
     }
 }
