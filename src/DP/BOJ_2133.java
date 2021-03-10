bb62fd762074d5d69e13c423adff68eb8e6afe62
yoooolim <agada937@naver.com>
2021-03-10 14:07:37 +0900

    Commit_middle

---
+++ src/DP/BOJ_2133.java
@@ -0,0 +1,17 @@
+package DP;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_2133 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        if(n%2==1) System.out.print(0);
+        else{
+            int[]dp=new int[n];
+            dp[2]=3;
+            dp[4]=15;
+        }
+    }
+}
---
+++ src/DP/BOJ_2225.java
@@ -0,0 +1,21 @@
+package DP;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_2225 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken(" "));
+        int k = Integer.parseInt(st.nextToken(" "));
+        long[][] dp = new long[2][201];
+        for(int i=0;i<k;i++) dp[0][i]=i+1;
+        for(int i=1;i<n;i++){
+            dp[i%2][0]=1;
+            for(int j=1;j<k;j++) dp[i%2][j]=(dp[i%2][j-1]+dp[(i-1)%2][j])%1000000000;
+        }
+        System.out.print(dp[(n-1)%2][k-1]);
+    }
+}
