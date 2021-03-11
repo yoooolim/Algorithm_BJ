c6db8ed557fe93d92ffb881c86d3f1eb84e7ab75
yoooolim <agada937@naver.com>
2021-03-11 15:44:32 +0900

    210311

---
+++ src/DP/BOJ_2011.java
@@ -0,0 +1,11 @@
+package DP;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_2011 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+    }
+}
