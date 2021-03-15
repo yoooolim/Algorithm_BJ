79670da0c098f39ab648f6e25c0de5be4794e2f5
yoooolim <agada937@naver.com>
2021-03-15 16:08:38 +0900

    210315

---
+++ src/Graph/BOJ_1206_DFSandBFS.java
@@ -0,0 +1,22 @@
+package Graph;
+
+import com.sun.corba.se.impl.orbutil.graph.Graph;
+import com.sun.corba.se.impl.orbutil.graph.Node;
+import com.sun.corba.se.impl.orbutil.graph.NodeData;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class BOJ_1206_DFSandBFS {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken(" "));
+        int m = Integer.parseInt(st.nextToken(" "));
+        int s = Integer.parseInt(st.nextToken(" "));
+    }
+}
