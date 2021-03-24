1fa36a9331bfd22230d1e84e2f6ea2df9731c566
yoooolim <agada937@naver.com>
2021-03-25 01:20:39 +0900

    210325commit

---
+++ src/Baekjoon/BinarySearch_and_TernarySearch/BOJ_2805_CutTree.java
@@ -0,0 +1,20 @@
+package Baekjoon.BinarySearch_and_TernarySearch;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class BOJ_2805_CutTree {
+    static private int treeN=0, getN=0;
+    static private ArrayList<Long> threeH = new ArrayList<>();
+
+    public static void main(String[] args){
+
+    }
+    public static void partIO() throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        treeN = Integer.parseInt(st.nextToken(" "));
+    }
+}
