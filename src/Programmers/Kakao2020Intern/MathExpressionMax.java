ee4d4679ece0f432248263ac3298784100e049fc
yoooolim <agada937@naver.com>
2021-04-12 13:48:12 +0900

    210412COMMIT

--- src/Programmers/Kakao2020Intern/MathExpressionMax.java
+++ src/Programmers/Kakao2020Intern/MathExpressionMax.java
@@ -1,30 +1,37 @@
 package Programmers.Kakao2020Intern;
 
+import java.util.ArrayList;
+
 public class MathExpressionMax {
     public static void main(String[] args) throws Exception{
         String expression = "100-200*300-500+20";
         solution(expression);
     }
+
     public static long solution(String expression){
         long answer = 0;
+        ArrayList<Integer> operator = new ArrayList<>();
+        ArrayList<Integer> number = new ArrayList<>();
+        // 숫자, operator 구분
+        seperate_oper_num(expression,operator,number);
+        
+        return answer;
+    }
+    public static int seperate_oper_num(String expression,ArrayList<Integer> operator,
+                                         ArrayList<Integer> number){
+        int result = 0;
         String[] str = expression.split("[+*-]");
-        int[] operator = new int[str.length-1];
-        int[] number = new int[str.length];
         int sum=0;
         for(int i=0;i<str.length-1;i++){
-            number[i]=Integer.parseInt(str[i]);
-            sum+=Math.log10(number[i])+1;
-            operator[i]=toOperator(expression.charAt(sum++));
+            number.add(Integer.parseInt(str[i]));
+            sum+=Math.log10(number.get(i))+1;
+            operator.add(toOperator(expression.charAt(sum++)));
         }
-        number[str.length-1]=Integer.parseInt(str[str.length-1]);
-
-        for(int i=0;i<number.length;i++){
-            System.out.print(number[i]+"\n");
-        }
-        for(int i=0;i<operator.length;i++){
-            System.out.print(operator[i]+"\n");
-        }
-        return answer;
+        number.add(Integer.parseInt(str[str.length-1]));
+        if(operator.indexOf(0)!=-1) result+=100;
+        if(operator.indexOf(1)!=-1) result+=10;
+        if(operator.indexOf(2)!=-1) result+=1;
+        return result;
     }
     public static int toOperator(char c){
         int result = -1;
