d21bc8ddcb0e4258d4094a7a8c86ec9b6d9d5982
yoooolim <agada937@naver.com>
2021-05-27 09:14:02 +0900

    210527 middle commit

---
+++ src/Baekjoon/Graph/BOJ_1655_SayCenter.java
@@ -0,0 +1,17 @@
+package Baekjoon.Graph;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class BOJ_1655_SayCenter {
+    static int inputNum;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        inputNum = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+
+    }
+
+}
